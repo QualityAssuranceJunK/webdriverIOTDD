c774d942346e31e593eaab88598a0e16bf25e9a1
junk <127572752+QualityAssuranceJunK@users.noreply.github.com>
2023-08-03 10:40:54 +0900

    Update test.e2e.ts

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,12 +1,12 @@
 import LoginStep from '../stepDefs/login.page.steps.js'
-import SecurePage from '../stepDefs/secure.page.js'
+import SecurePage from '../stepDefs/secure.page.step.js'
 
 describe('My Login application', function(){
     it('should login with valid credentials', async () => {
         await LoginStep.open()
-        await LoginStep.login('tomsmith', 'SuperSecretPassword!')
-        await expect(SecurePage.flashAlert).toBeExisting()
-        await expect(SecurePage.flashAlert).toHaveTextContaining('You logged into a secure area!')
-    }, 2)
+        await LoginStep.login('tomsmith', 'SuperSecretPassword!');
+        await SecurePage.containCheck('You logged into a secure area!');
+        await SecurePage.logOut();
+    })
 })
 
